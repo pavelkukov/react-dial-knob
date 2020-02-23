75079cd47d361db98ee8736c341a7eb3125dff46
Pavel Kukov <extpaku@pandora.net>
2020-02-23 12:21:39 +0200

    fix: add missing comma

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   stories: ['../stories/**/*.stories.tsx'],
   addons: [
-      '@storybook/addon-links/register'
+      '@storybook/addon-links/register',
       '@storybook/addon-knobs/register',
       '@storybook/addon-notes/register-panel'
   ],

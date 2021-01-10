11d102047b18642a430794ae79f800f6f0422566
Pavel Kukov <pavelkukov@gmail.com>
2021-01-10 13:18:43 +0200

    fix: include useAngleUpdater in core-only bundle

--- src/core-only.ts
+++ src/core-only.ts
@@ -2,6 +2,7 @@ import Knob, { KnobProps } from './Knob'
 import SkinProps from './skins/SkinProps'
 import SkinWrap from './skins/layout/SkinWrap'
 import composeTwo from './util/composeTwo'
+import useAngleUpdater from './util/useAngleUpdater'
 
 export default Knob
-export { SkinWrap, KnobProps, SkinProps, composeTwo }
+export { SkinWrap, KnobProps, SkinProps, composeTwo, useAngleUpdater }
